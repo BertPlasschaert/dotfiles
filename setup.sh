2aa886ae32cf4159d238f8c09f2b2328f4f5f000
Bert Plasschaert <bert.plasschaert@outlook.com>
2024-09-29 17:27:51 +0200

    added tmux plugin manager install

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ sudo snap install nvim --classic
 sudo apt-get install unzip
 sudo apt install python3-pip
 sudo apt install python3-venv
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
