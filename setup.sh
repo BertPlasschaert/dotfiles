befaafed61ea88d61e141da309659c4568c35348
Bert Plasschaert <bert.plasschaert@outlook.com>
2024-09-29 21:04:51 +0200

    added npm req to setup file

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ sudo snap install nvim --classic
 sudo apt-get install unzip
 sudo apt install python3-pip
 sudo apt install python3-venv
+sudo apt install npm 
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
